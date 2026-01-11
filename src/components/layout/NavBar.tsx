439118350fed880ad2a2a67e7e0da6121d79f1fc
donovanchen <donovanachen@gmail.com>
2026-01-11 14:20:37 -0800

    nav bar styling with bootstrap

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -5,19 +5,19 @@ function NavBar() {
   return (
     <header>
       {/* Navigation Bar */}
-      <nav>
+      <nav className="d-flex align-items-baseline justify-content-between px-5 py-4">
         {/* name button */}
         <div className="logo">
-          <Link to="/">Donovan Chen</Link>
+          <Link className="logo-name h4 text-decoration-none text-dark" to="/">Donovan Chen</Link>
         </div>
 
         {/* nav buttons */}
-        <ul className="nav_buttons">
+        <ul className="nav-links d-flex list-unstyled m-0 gap-4">
           <li>
-            <Link to="/projects">Projects</Link>
+            <Link to="/projects" className="text-decoration-none text-dark">Projects</Link>
           </li>
           <li>
-            <a href={resume} target="_blank">
+            <a className="text-decoration-none text-dark" href={resume} target="_blank">
               Resumé
             </a>
           </li>
