0d3937fd01215288529cf9066b644d96b0b97e11
donovanchen <donovanachen@gmail.com>
2026-01-10 19:13:39 -0800

    project details abstraction

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -1,15 +1,5 @@
 import { Link } from "react-router-dom";
-import fabflix_thumbnail from "../../../public/projects/fabflix/login.png";
-import glub_world_thumbnail from "../../../public/projects/glub_world/login.png";
-import guidance_glasses_thumbnail from "../../../public/projects/guidance_glasses/guidance_glasses.png";
-import purin_car_thumbnail from "../../../public/projects/purin_car/purin_car_records.png";
-
-interface Project {
-  title: string;
-  description: string;
-  thumbnail: string;
-  link: string;
-}
+import { projects, type Project} from "../../data/project"
 
 function ProjectCardItem({ title, description, thumbnail, link }: Project) {
   return (
@@ -27,37 +17,6 @@ function ProjectCardItem({ title, description, thumbnail, link }: Project) {
 }
 
 function ProjectCard() {
-  const projects: Project[] = [
-    {
-      title: "Purin Car",
-      description:
-        "Android application using Kotlin, Room Database and Jetpack Compose Android app for tracking vehicle maintenance intervals with a reactive UI.",
-      thumbnail: purin_car_thumbnail,
-      link: "/project/purin-car",
-    },
-    {
-      title: "Glub World",
-      description:
-        "Video game website using HTML, JavaScript, and CSS with AWS for my significant other.",
-      thumbnail: glub_world_thumbnail,
-      link: "/project/glub-world",
-    },
-    {
-      title: "Fabflix",
-      description:
-        "Movie catalog website using Java Servlets, MySQL, HTML, JavaScript/jQuery, and CSS with AWS, Maven, and Docker.",
-      thumbnail: fabflix_thumbnail,
-      link: "/project/fabflix",
-    },
-    {
-      title: "Guidance Glasses",
-      description:
-        "A pair of glasses that can assist the visually impaired to navigate urban environments by providing information about their surroundings via haptic feedback.",
-      thumbnail: guidance_glasses_thumbnail,
-      link: "/project/guidance-glasses",
-    },
-  ];
-
   return (
     <section className="projects">
       <h1>Projects</h1>
---
+++ src/data/project.ts
@@ -0,0 +1,44 @@
+import fabflix_thumbnail from "../../../public/projects/fabflix/login.png";
+import glub_world_thumbnail from "../../../public/projects/glub_world/login.png";
+import guidance_glasses_thumbnail from "../../../public/projects/guidance_glasses/guidance_glasses.png";
+import purin_car_thumbnail from "../../../public/projects/purin_car/purin_car_records.png";
+
+export interface Project {
+  title: string;
+  description: string;
+  thumbnail: string;
+  link: string;
+}
+
+export const projects: Project[] = [
+    {
+      title: "Purin Car",
+      description:
+        "Android application using Kotlin, Room Database and Jetpack Compose Android app for tracking vehicle maintenance intervals with a reactive UI.",
+      thumbnail: purin_car_thumbnail,
+      link: "/project/purin-car",
+    },
+    {
+      title: "Glub World",
+      description:
+        "Video game website using HTML, JavaScript, and CSS with AWS for my significant other.",
+      thumbnail: glub_world_thumbnail,
+      link: "/project/glub-world",
+    },
+    {
+      title: "Fabflix",
+      description:
+        "Movie catalog website using Java Servlets, MySQL, HTML, JavaScript/jQuery, and CSS with AWS, Maven, and Docker.",
+      thumbnail: fabflix_thumbnail,
+      link: "/project/fabflix",
+    },
+    {
+      title: "Guidance Glasses",
+      description:
+        "A pair of glasses that can assist the visually impaired to navigate urban environments by providing information about their surroundings via haptic feedback.",
+      thumbnail: guidance_glasses_thumbnail,
+      link: "/project/guidance-glasses",
+    },
+  ];
+
+  
