e66b278ddc8546b6f89359936c905195eee8f229
donovanchen <donovanachen@gmail.com>
2026-01-11 14:20:51 -0800

    format navbar doc

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -8,16 +8,24 @@ function NavBar() {
       <nav className="d-flex align-items-baseline justify-content-between px-5 py-4">
         {/* name button */}
         <div className="logo">
-          <Link className="logo-name h4 text-decoration-none text-dark" to="/">Donovan Chen</Link>
+          <Link className="logo-name h4 text-decoration-none text-dark" to="/">
+            Donovan Chen
+          </Link>
         </div>
 
         {/* nav buttons */}
         <ul className="nav-links d-flex list-unstyled m-0 gap-4">
           <li>
-            <Link to="/projects" className="text-decoration-none text-dark">Projects</Link>
+            <Link to="/projects" className="text-decoration-none text-dark">
+              Projects
+            </Link>
           </li>
           <li>
-            <a className="text-decoration-none text-dark" href={resume} target="_blank">
+            <a
+              className="text-decoration-none text-dark"
+              href={resume}
+              target="_blank"
+            >
               Resumé
             </a>
           </li>
